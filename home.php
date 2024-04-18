a277113081a5589500f8c97869e725b76e35c61e
bed-com-20-19x <bed-com-20-19@unima.ac.mw>
2024-04-18 05:08:59 +0200

    adding home page file

---
+++ home.php
@@ -0,0 +1,6 @@
+<!-- <hi><?php echo $title;?> -->
+
+<div class ="col-lg-12">
+    <h2>Mandata</h2>
+    <p>The mandate of Aphunzire e-learning website is to revolutionize education accessibility by offering a diverse range of courses, fostering interactive and engaging learning environments, empowering learners to pursue their educational goals at their own pace, and promoting lifelong learning opportunities for all individuals, regardless of geographical, socioeconomic, or physical barriers.</p>
+</div>
