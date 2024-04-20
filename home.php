2956d6dc2266f7a309be83a4dec8cb57866ec6b7
bed-com-20-19x <bed-com-20-19@unima.ac.mw>
2024-04-20 02:37:39 +0200

    modifying home page file

--- home.php
+++ home.php
@@ -15,6 +15,41 @@
      encouraging collaboration and knowledge sharing. 
     Through continuous improvement and adaptation, we aspire to meet the evolving needs of learners and educators.
     Ultimately, we envision a world where education is a fundamental right, enabling individuals to unlock their full potential and contribute positively to society.</p>
+
+    <h2>Goals</h2>
+    <ol>
+        <li>Promote inclusive Learning</li>
+        <li>Enhance Learning Experience</li>
+        <li>Facilitate Collaboration</li>
+        <li>Expand Access</li>
+    </ol>
+
+    <h2>Objectives</h2>
+    <ol>
+        <li>Foster an inclusive learning environment that accommodates learners from diverse backgrounds, cultures, and abilities, ensuring everyone feels welcome and valued.</li>
+        <li>Increase accessibility to education by reaching underserved communities and individuals with limited resources or geographical barriers.</li>
+        <li>Facilitate collaboration and knowledge-sharing among learners and educators through forums, discussion boards, and collaborative projects.</li>
+        <li>Expand the range of educational content to cover diverse subjects, skill levels, and learning styles, catering to the varied needs and interests of learners.</li>
+    </ol>
+    </div>
+
+    <div class="col-lg-6">
+     <h2>Guiding principles</h2>
+     <ol>
+        <li>Accessibility and inclusivity</li>
+        <li>partneship and collaboration</li>
+        <li>User-centric Approach</li>
+     </ol>
+
+     <h2>Core Values</h2>
+     <ol>
+        <li>Accessibility</li>
+        <li>Inclusivity</li>
+        <li>Collaboration</li>
+        <li>Empowerment</li>
+        <li>Excellence</li>
+     </ol>
+
     </div>
     
 </div>
