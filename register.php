1ff7b30cddf544433db52cacc2374b631c0a8557
bed-com-20-19x <bed-com-20-19@unima.ac.mw>
2024-04-22 03:40:33 +0200

    adding register file and deleting some file

--- register.php
+++ register.php
@@ -47,7 +47,7 @@ if (isset($_SESSION['StudentID'])){
                     <label class="col-md-4 control-label" for="FNAME">Fisrt Name:</label>
 
                     <div class="col-md-8">
-                        <input class="form-control input-sm" id="FNAME" placeholder="First Name" type="text" value="required">
+                        <input class="form-control input-sm" id="FNAME" placeholder="First Name" type="text" value="" required>
                     </div>
                 </div>
             </div>
@@ -57,12 +57,58 @@ if (isset($_SESSION['StudentID'])){
                     <label class="col-md-4 control-label" for="LNAME">Last Name:</label>
 
                     <div class="col-md-8">
-                        <input class="form-control input-sm" id="LNAME" name="LNAME" placeholder="Last Name" type="text" value="required">
+                        <input class="form-control input-sm" id="LNAME" name="LNAME" placeholder="Last Name" type="text" value="">
 
                     </div>
                 </div>
             </div>
 
-            </form>
-        </div>
-        </div>
+            <div class="form-group">
+                <div class="col-md-9">
+                    <label class="col-md-4 control-label" for="PASS">Password</label>
+
+                    <div class="col-md-8">
+                        <input class="form-control input-sm" id="PASS" name="PASS" placeholder="Password" type="password" value="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-9">
+                    <label class="col-md-4 control-label" for="idno"></label>
+
+                    <div class="col-md-8">
+                        <button type="submit" name="btnRegister" class="btn btn-primary btn-sm">Register</button>
+                        <a href="login.php"><i class="fa fa-arrow-left"></i>Back to Login</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </form>
+    </div>
+</div>
+
+<?php 
+if (isset($_POST['btnRegister'])){
+   #code ...
+   
+   $student = new Student();
+   $student->Fname    = $_POST['FNAME'];
+   $student->Lname    = $_POST['LNAME'];
+   $student->Address    = $_POST['ADDRESS'];
+   $student->MobileNo    = $_POST['PHONE'];
+   $student->STUDUSERNAME    = $_POST['USERNAME'];
+   $student->STUDPASS    = sha1($_POST['PASS']);
+   $student->create();
+
+   message("Your now succefully registere. you can login now!", "success");
+   redirect("register.php");
+}
+
+?>
+<footer style="height: 100px; border-top: 1px solide #ddd; padding: 5px; background-color: blue; color:#fff">
+        <p align="left">&copy; MOVING FORWARD WITH EDUCATION</p>
+</footer>
+</section>
