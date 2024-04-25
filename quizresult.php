5426c3101254db480e47580ffa936e53a6853bd7
bed-com-10-19 <bed-com-10-19@unima.ac.mw>
2024-04-25 22:24:14 +0200

    manage quizresult

--- quizresult.php
+++ quizresult.php
@@ -13,3 +13,22 @@ $id = $_GET['id'];
 if ($id == '') {
     redirect("index.php");
 }
+
+$sql = "SELECT * FROM tblexercise WHERE LessonID = '{$id}'";
+$mydb->setQuery($sql);
+$cur = $mydb->loadResultList();
+
+foreach ($cur as $res) {
+    $sql = "SELECT * FROM tblscore s,tblexercise e WHERE s.ExerciseID=e.ExerciseID AND e.ExerciseID='{$res->ExerciseID}' and StudentID='{$studentid}'";
+    $mydb->setQuery($sql);
+    $ans = $mydb->loadSingleResult();
+?>
+    <form> 
+        <div><?php echo $res->Question; ?></div>
+        <div class="col-md-3"><input class="radios" type="radio" disabled="true" <?php echo ($ans && $ans->Answer == $res->ChoiceA) ? 'CHECKED' : ''; ?>> A. <?php echo $res->ChoiceA; ?></div>
+        <div class="col-md-3"><input class="radios" type="radio" disabled="true" <?php echo ($ans && $ans->Answer == $res->ChoiceB) ? 'CHECKED' : ''; ?>> B. <?php echo $res->ChoiceB; ?></div>
+        <div class="col-md-3"><input class="radios" type="radio" disabled="true" <?php echo ($ans && $ans->Answer == $res->ChoiceC) ? 'CHECKED' : ''; ?>> C. <?php echo $res->ChoiceC; ?></div>
+        <div class="col-md-3"><input class="radios" type="radio" disabled="true" <?php echo ($ans && $ans->Answer == $res->ChoiceD) ? 'CHECKED' : ''; ?>> D. <?php echo $res->ChoiceD; ?></div> 
+    </form>
+<?php } ?>
+</div>
