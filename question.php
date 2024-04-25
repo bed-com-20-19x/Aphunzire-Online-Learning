da85ac0841e64a10acff0f074490ca45dec0f3ae
bed-com-10-19 <bed-com-10-19@unima.ac.mw>
2024-04-25 21:12:17 +0200

    Continue adding content to question page

--- question.php
+++ question.php
@@ -5,3 +5,9 @@ $id = $_GET['id'];
 if($id==''){
 redirect("index.php");
 }
+
+$sql = "SELECT SUM(Score) as 'SCORE' FROM tblscore  WHERE LessonID='{$id}' and StudentID='{$studentid}' AND Submitted=1";
+	$mydb->setQuery($sql);
+	$row = $mydb->executeQuery(); 
+    $ans = $mydb->loadSingleResult();
+    $score  = $ans->SCORE;
