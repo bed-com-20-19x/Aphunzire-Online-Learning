ad1c04c4af4d43b535a4e8db9be96486b58a25c7
bed-com-20-19x <bed-com-20-19@unima.ac.mw>
2024-05-02 17:03:54 +0200

    adding new file assignment_sidebar.php

---
+++ admin/assignment_sidebar.php
@@ -0,0 +1,46 @@
+<div class="span3" id="sidebar">
+    <ul class="nav mav-list bs-dosc-sidenav nav-collapse collapse">
+        <li><a href="dashboard.php"><i class="icon-chevron-right"></i><i class="icon-home"></i>&nbsp;Dashboard</a></li>
+        <li>
+            <a href="subject.php"><i class="icon-chevron-right"></i><i class="icon-list-alt"></i>subject</a>
+        </li>
+        <li>
+            <a href="class.php"><i class="icon-chevron-right"></i><i class="icon-group"></i>class</a>
+        </li>
+        <li>
+            <a href="admin_user.php"><i class="icon-chevron-right"></i><i class="icon-user"></i>Admin Users</a>
+        </li>
+        <li>
+            <a href="department.php"><i class="icon-chevron-right"></i><i class="icon-building"></i>Department</a>
+        </li>
+        <li>
+            <a href="students.php"><i class="icon-chevron-right"></i><i class="icon-group"></i>students</a>
+        </li>
+        <li>
+            <a href="teacher.php"><i class="icon-chevron-right"></i><i class="icon-group"></i>Teacher</a>
+        </li>
+        <li>
+            <a href="dowloadable.php"><i class="icon-chevron-right"></i><i class="icon-download"></i>Downloadable Materials</a>
+        </li>
+        <li class="active">
+            <a href="assignment.php"><i class="icon-chevron-right"></i><i class="icon-upload"></i>uploaded Assignment</a>
+        </li>
+        <li>
+            <a href="content.php"><i class="icon-chevron-right"></i><i class="icon-file"></i>content</a>
+        </li>
+        <li>
+            <a href="user_log.php"><i class="icon-chevron-right"></i><i class="icon-file"></i>User Log</a>
+        </li>
+        <li>
+            <a href="activity_log.php"><i class="icon-chevron-right"></i><i class="icon-file"></i>Activity Log</a>
+        </li>
+        <li>
+            <a href="school_year.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i>School Year</a>
+        </li>
+        <li>
+            <a href="calendar_of_events.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i>Calendar of Events</a>
+        </li>
+
+    </ul>
+
+</div>
