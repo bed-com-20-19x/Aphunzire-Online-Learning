8728a18404b607a42e0e719bec6b18c45f091324
Theodore Chilongo <theodorechilongo@gmail.com>
2024-05-02 15:52:46 +0200

    added class deleting functionality

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,7 +1,9 @@
+<!-- dashboard database connection -->
 <?php
-// connecting to  the database
-include('dbcon.php');
-$get_id = $_GET['id'];
-mysqli_query($conn,"update teacher set teacher_stat = 'Deactivated' where teacher_id = '$get_id'");
-header('location:teachers.php');
+
+// connection code to database
+$conn = mysqli_connect('localhost','root','','capstone') or die(mysqli_error());
 ?>
+
+
+
